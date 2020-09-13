14151cea58ee556d61e69657252dc623c92aa064
Mikhail Shamshurin <37084028+6eremotuk01@users.noreply.github.com>
2020-09-13 16:30:52 +0300

    Добавлена возможность добавления элемента

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import TableWrapper from "./Table/TableWrapper";
 import ShowInformation from "./Information/ShowInformation";
-import AddNewRowButton from "./Table/addNewRowButton";
+import AddNewRow from "./Table/addNewRow";
 
 function App() {
     const queryURL = `http://www.filltext.com/?rows=10&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D`;
@@ -13,16 +13,12 @@ function App() {
         fetch(queryURL)
             .then((res) => res.json())
             .then((data) => setRows(data));
-    }, []);
+    }, [queryURL]);
 
     return (
         <div className="wrapper">
-            <div className="header">
-                <AddNewRowButton
-                    rows={rows}
-                    setRows={setRows}
-                />
-            </div>
+            <h1 className="wrapper__header">Таблица</h1>
+            <AddNewRow rows={rows} setRows={setRows} />
             <TableWrapper
                 rows={rows}
                 onRowClick={setSelectedRow}
---
+++ src/Table/addNewRow.js
@@ -0,0 +1,132 @@
+import React from "react";
+
+class AddNewRow extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.defaultValue = this.state = {
+            id: "",
+            firstName: "",
+            lastName: "",
+            email: "",
+            phone: "",
+        };
+
+        this.fills = [];
+        for (let item in this.state) {
+            this.fills.push(item);
+        }
+
+        this.setNewValue = this.setNewValue.bind(this);
+        this.addNewItem = this.addNewItem.bind(this);
+    }
+
+    addNewItem(event) {
+        let address = {
+            streetAddress: "Не указано",
+            city: "Не указано",
+            state: "Не указано",
+            zip: "Не указано",
+        };
+        let description = "Не указано";
+
+        this.props.setRows([
+            {
+                id: this.state.id,
+                firstName: this.state.firstName,
+                lastName: this.state.lastName,
+                email: this.state.email,
+                phone: this.state.phone,
+                address: address,
+                description: description,
+            },
+            ...this.props.rows,
+        ]);
+
+        this.setState(this.defaultValue);
+
+        event.target.reset();
+        event.preventDefault();
+    }
+
+    setNewValue(event, name) {
+        this.setState({ [name]: event.target.value });
+    }
+
+    render() {
+        if (!this.state.isOpened) {
+            return (
+                <div className="add-wrapper">
+                    <button
+                        className="add-wrapper__open-button--is-not-opened"
+                        onClick={() =>
+                            this.setState({ isOpened: !this.state.isOpened })
+                        }
+                    >
+                        + Новая запись
+                    </button>
+                </div>
+            );
+        } else {
+            return (
+                <div className="add-wrapper">
+                    <button
+                        className="add-wrapper__open-button"
+                        onClick={() =>
+                            this.setState({ isOpened: !this.state.isOpened })
+                        }
+                    >
+                        × Отмена
+                    </button>
+                    <form
+                        className="add-wrapper__form"
+                        onSubmit={(e) => this.addNewItem(e)}
+                    >
+                        <div className="add-wrapper__form__parameters">
+                            <div className="add-wrapper__form__parameters__headers-wrapper">
+                                {this.fills.map((key) => (
+                                    <p
+                                        key={key}
+                                        className="add-wrapper__form__parameters__headers-wrapper__header"
+                                    >
+                                        {key}
+                                    </p>
+                                ))}
+                            </div>
+                            <div className="add-wrapper__form__parameters__fills-wrapper">
+                                {this.fills.map((key) => (
+                                    <input
+                                        className="add-wrapper__form__parameters__fills-wrapper__fill"
+                                        key={key}
+                                        type="text"
+                                        value={this.state[key]}
+                                        onChange={(e) =>
+                                            this.setNewValue(e, key)
+                                        }
+                                        autoComplete="off"
+                                        required
+                                    />
+                                ))}
+                            </div>
+                        </div>
+                        <div className="add-wrapper__form__buttons-wrapper">
+                            <input
+                                className="add-wrapper__form__buttons-wrapper__clear-button"
+                                type="reset"
+                                value="Очистить форму"
+                                onClick={() => this.setState(this.defaultValue)}
+                            />
+                            <input
+                                className="add-wrapper__form__buttons-wrapper__add-button"
+                                type="submit"
+                                value="+ Добавить"
+                            />
+                        </div>
+                    </form>
+                </div>
+            );
+        }
+    }
+}
+
+export default AddNewRow;
